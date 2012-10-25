e2cc5768ec82b165ca6e3f4e675b370f686a4201
Edward Muller <edwardam@interlix.com>
2012-10-25 15:09:45 -0700

    Update postgres adapter for current regsitry api

--- lib/dcell/registries/postgres_adapter.rb
+++ lib/dcell/registries/postgres_adapter.rb
@@ -4,32 +4,32 @@ require 'uri'
 # CREATE DATABASE dcell_test;
 # CREATE USER dcell NOSUPERUSER NOCREATEDB NOCREATEROLE;
 # \c dcell_test
-# CREATE TABLE keys (key text, value text, PRIMARY KEY(key));
-# GRANT INSERT, SELECT, UPDATE, TRUNCATE ON TABLE keys TO dcell;
+# CREATE TABLE registry(type text, key text, value text, PRIMARY KEY(type, key));
+# GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE registry TO dcell;
 #
-# CREATE OR REPLACE FUNCTION public.dcell_upsert(k text, v text)
-#  RETURNS void
-#  LANGUAGE plpgsql
-#  AS $function$
-#  BEGIN
-#      LOOP
-#          -- first try to update the key
-#          UPDATE keys SET value = v WHERE key = k;
-#          IF found THEN
-#              RETURN;
-#          END IF;
-#          -- not there, so try to insert the key
-#          -- if someone else inserts the same key concurrently,
-#          -- we could get a unique-key failure
-#          BEGIN
-#              INSERT INTO keys(key, value) VALUES (k, v);
-#              RETURN;
-#          EXCEPTION WHEN unique_violation THEN
-#              -- Do nothing, and loop to try the UPDATE again.
-#          END;
-#      END LOOP;
-#  END;
-#  $function$;
+#CREATE OR REPLACE FUNCTION public.dcell_upsert(t text, k text, v text)
+# RETURNS void
+#LANGUAGE plpgsql
+#AS $function$
+# BEGIN
+#     LOOP
+#         -- first try to update the key
+#         UPDATE registry SET value = v WHERE type = t and key = k;
+#         IF found THEN
+#             RETURN;
+#         END IF;
+#         -- not there, so try to insert the key
+#         -- if someone else inserts the same key concurrently,
+#         -- we could get a unique-key failure
+#         BEGIN
+#             INSERT INTO registry(type, key, value) VALUES (t, k, v);
+#             RETURN;
+#         EXCEPTION WHEN unique_violation THEN
+#             -- Do nothing, and loop to try the UPDATE again.
+#         END;
+#     END LOOP;
+# END;
+# $function$;
 
 module DCell
   module Registry
@@ -41,16 +41,40 @@ module DCell
         url = options[:url] || ENV['DATABASE_URL']
         @uri = URI.parse(url)
 
+        @node_registry   = NodeRegistry.new(pg_conn)
         @global_registry = GlobalRegistry.new(pg_conn)
       end
 
-      def clear_globals
-        @global_registry.clear
+      class NodeRegistry
+        def initialize(pg)
+          @pg = pg
+        end
+
+        def get(node_id)
+          result = @pg.exec('SELECT value FROM registry WHERE type = $1::text and key = $2::text', [{value: 'nodes', format: 0, type: 0},{value: node_id, format: 0, type: 0}])
+          result[0]["value"] if result[0]["value"]
+        end
+
+        def set(node_id, addr)
+          @pg.exec('SELECT dcell_upsert($1::text, $2::text, $3::text)', [{value: 'nodes', format: 0, type: 0},
+                                                                         {value: node_id, format: 0, type: 0},
+                                                                         {value: addr, format: 0, type: 0}])
+        end
+
+        def nodes
+          result = @pg.exec("SELECT key FROM registry where type = 'nodes'")
+          result.map { |i| i["key"] }
+        end
+
+        def clear
+          @pg.exec("DELETE FROM registry WHERE type = 'nodes'")
+        end
       end
 
-      def get_global(key);        @global_registry.get(key) end
-      def set_global(key, value); @global_registry.set(key, value) end
-      def global_keys;            @global_registry.global_keys end
+      def get_node(node_id);       @node_registry.get(node_id) end
+      def set_node(node_id, addr); @node_registry.set(node_id, addr) end
+      def nodes;                   @node_registry.nodes end
+      def clear_nodes;             @node_registry.clear end
 
       class GlobalRegistry
         def initialize(pg)
@@ -58,23 +82,31 @@ module DCell
         end
 
         def get(key)
-          result = @pg.exec('SELECT value FROM keys WHERE key = $1::text', [{value: key, format: 0, type: 0}])
+          result = @pg.exec('SELECT value FROM registry WHERE type = $1::text and key = $2::text', [{value: 'globals', format: 0, type: 0},
+                                                                                                    {value: key, format: 0, type: 0}])
           Marshal.load result[0]["value"] if result[0]["value"]
         end
 
         def set(key, value)
-          result = @pg.exec('SELECT dcell_upsert($1::text, $2::text);', [{value: key, format: 0, type: 0},{value: Marshal.dump(value), format: 0, type: 0}])
+          @pg.exec('SELECT dcell_upsert($1::text, $2::text, $3::text)', [{value: 'globals', format: 0, type: 0},
+                                                                         {value: key, format: 0, type: 0},
+                                                                         {value: Marshal.dump(value), format: 0, type: 0}])
         end
 
         def global_keys
-          result = @pg.exec('SELECT key from keys')
+          result = @pg.exec('SELECT key FROM registry WHERE type = \'globals\'')
           result.map { |i| i["key"] }
         end
 
         def clear
-          @pg.exec('TRUNCATE keys')
+          @pg.exec("DELETE FROM registry WHERE type = 'globals'")
         end
       end
+
+      def get_global(key);        @global_registry.get(key) end
+      def set_global(key, value); @global_registry.set(key, value) end
+      def global_keys;            @global_registry.global_keys end
+      def clear_globals;          @global_registry.clear end
 
       private 
       def pg_conn
@@ -84,7 +116,7 @@ module DCell
           dbname:   @uri.path[1..-1],
           password: @uri.password,
           port:     @uri.port,
-          #sslmode:  'require',
+          sslmode:  'prefer',
           connect_timeout: 20
         )
       end
--- spec/support/registry_examples.rb
+++ spec/support/registry_examples.rb
@@ -2,6 +2,7 @@ shared_context "a DCell registry" do
   context "node registry" do
     before :each do
       subject.clear_nodes
+      subject.nodes.should be_empty
     end
 
     it "stores node addresses" do
@@ -9,6 +10,16 @@ shared_context "a DCell registry" do
 
       subject.set_node("foobar", address)
       subject.get_node("foobar").should == address
+    end
+
+    it "updates node addresses" do
+      initial_address = 'tcp://localhost:7777'
+      subject.set_node("foobar", initial_address)
+      subject.get_node("foobar").should == initial_address
+
+      new_address = 'tcp://localhost:6666'
+      subject.set_node("foobar", new_address)
+      subject.get_node("foobar").should == new_address
     end
 
     it "stores the IDs of all nodes" do
@@ -20,11 +31,23 @@ shared_context "a DCell registry" do
   context "global registry" do
     before :each do
       subject.clear_globals
+      subject.global_keys.should be_empty
     end
 
     it "stores values" do
-      subject.set_global("foobar", [1,2,3])
-      subject.get_global("foobar").should == [1,2,3]
+      value = [1,2,3]
+      subject.set_global("foobar", value)
+      subject.get_global("foobar").should == value
+    end
+
+    it "updates values" do
+      initial_value = [1,2,3]
+      subject.set_global("foobar", initial_value)
+      subject.get_global("foobar").should == initial_value
+
+      new_value = [4,5,6]
+      subject.set_global("foobar", new_value)
+      subject.get_global("foobar").should == new_value
     end
 
     it "stores the keys of all globals" do
